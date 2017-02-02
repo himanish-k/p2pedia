7912afe923a769e0a1576593921e129c9ea42956
himanish <himanish.kaushal@gmail.com>
2017-02-02 18:48:15 -0500

    When navigating to document-new section of the website, the navigation menu disappears and a close button appears instead so that user cannot leave document-new section without choosing to save or discard changes made to new document. The editor is successfully constructed and destroyed when navigating to and away from the document-new section respectively. The form to create new document exists as a template.

--- js/menu-highlighter.js
+++ js/menu-highlighter.js
@@ -3,20 +3,70 @@ var SECTION = {
 		DASHBOARD : 'dashboard',
 		LIST 			: 'document-list',
 		VIEW 			: 'document-view',
+		NEW 			: 'document-new',
 		EDIT 	 		: '#edit-document',
-		PEERS			: '#peers',
-		SETTINGS	: '#settings'
+		PEER_LIST	: 'peer-list',
+		SETTINGS	: 'settings'
+};
+
+var navigation = { 
+	current : SECTION.NEW,
+	enabled : true
+};
+
+// Tasks performed according to preferences when the application
+// starts up.
+function startup() {
+	constructEditor();
+}
+
+// Changes current property of navigation object to the section to which
+// the application should navigate. AKA allows only one section to be 
+// visible at a time.
+function navigate(e) {
+	var section = null;
+	// If e.data exists, then it is a callback from a click event 
+	// triggered by a menu item.
+	if(e.data){
+		section = e.data.section;
 	}
-var navigation = { };
-navigation.current = SECTION.DASHBOARD;
+	// Otherwise the parameter is itself the section name.
+	else {
+		section = e;
+	}
+
+	$.observable(navigation).setProperty('current', section);
+}
+
+// Constructs an instance of CK Editor to create new document. Disables the
+// menu so that the user cannot directly navigate to another part of the 
+// application without choosing to save or abandon changes. Then navigates
+// to the document-new section.
+function constructEditor() {
+	CKEDITOR.replace('editor');
+	$.observable(navigation).setProperty('enabled', false);
+	navigate(SECTION.NEW);
+}
+
+// Destroys the instance of CK Editor. Enables the menu for navigation
+// purposes again. Then navigates to the dashboard section.
+function destroyEditor() {
+	CKEDITOR.instances.ckeditor.destroy();
+	$.observable(navigation).setProperty('enabled', true);
+	navigate(SECTION.DASHBOARD);
+}
+
 
 $(document).ready(function() {	
+
+	startup();
+
 	// cycle over all anchor elements in navigation sidebar to add click listener
-	$('.nav.nav-sidebar').find('a').each(function() {
+	$('.nav.nav-sidebar.menu').find('a').each(function() {
 		// add click listener 
 		$(this).click(function() {
 			// "deactivate" currently "active" list element
-			$('.nav.nav-sidebar').children('li.active').removeClass('active');
+			$('.nav.nav-sidebar.menu').children('li.active').removeClass('active');
 			// finally, "activate" required list element
 			$(this).parent('li').addClass('active');
 		});
@@ -25,29 +75,23 @@ $(document).ready(function() {
 	// Bind navigation variable to body.
 	$('body').link(true, navigation);
 
-	var documentViewTemplate = $.templates("#document-view-template")
-var documentView = {};	
-documentView.document = JSON.parse(localStorage['documents'])[0];
-documentViewTemplate.link('#document-view', documentView);
-
 	// Bind click event handlers for each item in navigation menu
 	$('a#nav-to-dashboard').click({ section : SECTION.DASHBOARD }, navigate);	
 	$('a#nav-to-document-list').click({ section : SECTION.LIST }, navigate);
+	$('a#nav-to-document-new').click(constructEditor);
+	$('a#nav-to-peer-list').click({ section : SECTION.PEER_LIST }, navigate);
+	$('a#nav-to-settings').click({ section : SECTION.SETTINGS }, navigate);
+	$('a#close-editor').click(destroyEditor);
 	
+	// Click event handler for all tables rows in document-list section.
 	$('tr.nav-to-document-view').click(function(e) {
+		// Fetch ID of the document in this row.
 		var id = $.view(this).data.id;
-		console.log('called' + id);
-		navigate({ data: { section : SECTION.VIEW } });
+		// Retrieve and set document content with specfied ID.
 		$.observable(documentView).setProperty('document', JSON.parse(localStorage['documents'])[id]);
-		console.log(documentView.document.content);
+		// Navigate to the document-view section.
+		navigate(SECTION.VIEW);
+
 	});
-
-	// Changes current property of navigation object to the section to which
-	// the application should navigate. AKA allows only one section to be 
-	// visible at a time.
-	function navigate(e) {
-		$.observable(navigation).setProperty('current', e.data.section);
-	}
-
 
 });
